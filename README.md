a0c81f5ed55dfb61a8a2d98cd2dac98a7e4b5946
Igardemi <ival69@hotmail.com>
2023-01-30 13:32:45 +0100

    feat(#01):preparo cliente para despliegue

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# emoveDAW
+# ProyectoDaw
---
+++ src/web/js/login.js
@@ -0,0 +1,49 @@
+let usuario = null;
+
+function getUsers() {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://localhost:97/api/usuarios");
+        xhr.responseType = "json";
+        xhr.send();
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                resolve(xhr.response);
+            } else {
+                reject("Error " + xhr.statusText);
+            }
+        };
+    });
+}
+//No se si pasarle por parametro el array
+function iniciarSesion(datos) {
+
+    let estaIniciado = false;
+    let nombre = document.getElementById("user");
+    let pass = document.getElementById("password");
+
+    if (nombre.value == "" && pass.value == "") {
+        alert("Introduce los dos datos");
+    } else {
+        datos.forEach((user) => {
+            if (user.nombre == nombre.value && user.password == pass.value) {
+                usuario=user;
+                estaIniciado=true;               
+                gethistorial();
+                document.getElementById("login").close();
+                document.getElementById("historialCarritos").showModal()
+            }
+        });
+        //probando
+        if (estaIniciado==true && carrito.articulos.length != 0) {
+                    guardarCarrito();
+        }
+        if(estaIniciado==false){
+            alert("Datos incorrectos")
+        }
+    }
+}
+
+function usoPromesa() {  
+    getUsers().then((datos) => iniciarSesion(datos)).catch((error) => console.log(error));
+}
